db010abecd11f124023ba20be3b5ecc059996d5c
Toco Tachibana <ttachibana@my.bcit.ca>
2022-03-16 22:03:50 -0700

    Load header and footer using JS

---
+++ scripts/global.js
@@ -0,0 +1,6 @@
+function loadSkeleton(){
+    console.log($('#navbarPlaceholder').load('../partials/header.html'));
+    console.log($('#footerPlaceholder').load('../partials/footer.html'));
+}
+
+loadSkeleton();
