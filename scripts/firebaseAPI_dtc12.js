8b43b845a4aaf6b189b4fd9d892f9d2980824159
Toco Tachibana <ttachibana@my.bcit.ca>
2022-03-16 23:41:59 -0700

    Complete login.html

---
+++ scripts/firebaseAPI_dtc12.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDfG_EnZlYF8Z9fUWNWIpIJy4p1WBIx2H4",
+  authDomain: "dtc-12.firebaseapp.com",
+  projectId: "dtc-12",
+  storageBucket: "dtc-12.appspot.com",
+  messagingSenderId: "68678619104",
+  appId: "1:68678619104:web:bf2c2fd10da35a19bda585"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
