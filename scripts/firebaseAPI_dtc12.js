d831f3e37e262b7c45068bd79013c220b2b65aff
Toco Tachibana <ttachibana@my.bcit.ca>
2022-03-18 02:21:58 -0700

    Incorporate firebase to populate event cards

--- scripts/firebaseAPI_dtc12.js
+++ scripts/firebaseAPI_dtc12.js
@@ -11,3 +11,33 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+
+function displayCards(collection) {
+  let eventTemplate = document.getElementById("eventTemplate");
+
+  db.collection(collection).get()
+    .then(snap => {
+      var i = 1;
+      snap.forEach(doc => { //iterate thru each doc
+        var title = doc.data().name;
+        var details = doc.data().details;
+        var image = doc.data().image;
+        let newcard = eventTemplate.content.cloneNode(true);
+
+        //update title and text and image
+        newcard.querySelector('.card-title').innerHTML = title;
+        newcard.querySelector('.card-text').innerHTML = details;
+        newcard.querySelector('.card-img').src = image;
+
+        //give unique ids to all elements for future use
+        //newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+        //newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+        //newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+        document.getElementById("eventsList").appendChild(newcard);
+        i++;
+      })
+    })
+}
+
+displayCards("events");
---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+async function readJSON() {
+  i = 0
+  const response = await fetch(
+    'https://sports.api.decathlon.com/sports'
+  )
+  const data = await response.text(); //get text file, string
+  const sportsEvents = JSON.parse(data); //convert to JSON
+  console.log(sportsEvents);
+  for (x of sportsEvents["data"]) { //iterate thru each hero
+    let name = x.attributes.name;
+    let details = x.attributes.description;
+    let image = x.relationships.images.data[0];
+    console.log(image);
+    i++
+    if (i <= 20) {
+      db.collection("events").add({
+        name: name,
+        details: details,
+        image: image["url"]
+      })
+    }
+  }
+}
